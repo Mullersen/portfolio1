d0e8bdc2b0ed957e7bc9ca87da361d9ee1b1c9f4
mullersen <mariannhaugland@gmail.com>
2020-05-20 00:40:11 +0000

    success form message displayed

--- portfolio1/src/views/Contact.vue
+++ portfolio1/src/views/Contact.vue
@@ -11,17 +11,30 @@
                   <div class="media-content">
                     <div class="title is-4">Mariann Haugland</div>
                     <div>
-                      <i class="fab fa-linkedin"></i><p class="subtitle is-6" style="display:inline-block; margin-left:0.75rem;" >
-                        <a href="https://www.linkedin.com/in/m-haugland-webdeveloper/">My Linkedin</a> 
+                      <i class="fab fa-linkedin"></i>
+                      <p class="subtitle is-6" style="display:inline-block; margin-left:0.75rem;">
+                        <a href="https://www.linkedin.com/in/m-haugland-webdeveloper/">My Linkedin</a>
                       </p>
                     </div>
                     <div>
-                      <i class="fas fa-envelope-square"></i><p class="subtitle is-6" style="display:inline-block; margin-left:0.75rem;">mariannhaugland@gmail.com</p>
+                      <i class="fas fa-envelope-square"></i>
+                      <p
+                        class="subtitle is-6"
+                        style="display:inline-block; margin-left:0.75rem;"
+                      >mariannhaugland@gmail.com</p>
                     </div>
                     <div>
-                      <i class="fas fa-phone-square"></i><p class="subtitle is-6" style="display:inline-block; margin-left:0.75rem;">+1 403 707 7241</p>
+                      <i class="fas fa-phone-square"></i>
+                      <p
+                        class="subtitle is-6"
+                        style="display:inline-block; margin-left:0.75rem;"
+                      >+1 403 707 7241</p>
                     </div>
-                    <button class="button" style="margin-top:1rem" @click="modalToggle">See my Resume</button>
+                    <button
+                      class="button"
+                      style="margin-top:1rem"
+                      @click="modalToggle"
+                    >See my Resume</button>
                   </div>
                 </div>
               </div>
@@ -35,20 +48,22 @@
                 <div class="media">
                   <div class="media-content">
                     <p class="title is-4">Help me build my portfolio</p>
-                    <p class="subtitle">Do you have a project for me? Let me know, and I will get back to you!</p>
+                    <p
+                      class="subtitle"
+                    >Do you have a project for me? Let me know, and I will get back to you!</p>
                     <form class="contact-form" @submit.prevent="sendEmail">
                       <label class="label">Your name</label>
                       <div class="control">
-                        <input class="input" type="text" name="from_name">
+                        <input class="input" type="text" name="from_name" />
                       </div>
                       <label class="label">Your email</label>
                       <div class="control">
-                        <input class="input" type="email" name="user_email">
+                        <input class="input" type="email" name="user_email" />
                       </div>
                       <label class="label">Your message</label>
                       <div class="control">
                         <textarea class="input" name="message_html"></textarea>
-                        <input class="button" type="submit" value="Send">
+                        <input class="button" type="submit" value="Send" />
                       </div>
                     </form>
                   </div>
@@ -59,10 +74,23 @@
         </div>
       </div>
     </div>
-    <div class="modal">
+    <div class="modal modal1">
       <div class="modal-background" @click="modalClose"></div>
       <div class="modal-content">
-        <iframe src="https://docs.google.com/document/d/e/2PACX-1vQOBfyrYl6NRK4iIalGgKVJG3Hlh-dr1TujDhYEdROrRpUSNojl4dIAAIudjDVu_9ftoP9_J1w4dmaC/pub?embedded=true" height="700px" width="800px"></iframe>
+        <iframe
+          src="https://docs.google.com/document/d/e/2PACX-1vQOBfyrYl6NRK4iIalGgKVJG3Hlh-dr1TujDhYEdROrRpUSNojl4dIAAIudjDVu_9ftoP9_J1w4dmaC/pub?embedded=true"
+          height="700px"
+          width="800px"
+        ></iframe>
+      </div>
+      <button @click="modalClose" class="modal-close is-large" aria-label="close"></button>
+    </div>
+    <div class="modal modal2">
+      <div class="modal-background" @click="modalClose"></div>
+      <div class="modal-content">
+        <div class="box">
+          <h2 id="modaltitle" class="title">Your message was sent successfully!</h2>
+        </div>
       </div>
       <button @click="modalClose" class="modal-close is-large" aria-label="close"></button>
     </div>
@@ -70,61 +98,83 @@
 </template>
 
 <script>
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export default {
+  data: function() {
+    return {
+      togglestate: false
+    };
+  },
   methods: {
-    modalToggle: function(){
-      var modal = document.getElementsByClassName('modal');
+    modalToggle: function() {
+      var modal = document.getElementsByClassName("modal1");
       modal.forEach(element => {
-        element.classList.add('is-active');
-      });
-    }, 
-    modalClose : function(){
-      var modal = document.getElementsByClassName('modal');
-      modal.forEach(element => {
-        element.classList.remove('is-active');
+        element.classList.add("is-active");
       });
     },
-    sendEmail: (e) => {
+    modalClose: function() {
+      var modal = document.getElementsByClassName("modal");
+      modal.forEach(element => {
+        element.classList.remove("is-active");
+      });
+    },
+    sendEmail: e => {
       emailjs.init("user_NN9nBpNBUHCW5dh5dryPi");
-      emailjs.sendForm('gmail', 'template_apcrZw0l', e.target, 'user_NN9nBpNBUHCW5dh5dryPi')
-        .then((result) => {
-            console.log('SUCCESS!', result.status, result.text);
-            var input = document.getElementsByClassName('input');
+      emailjs
+        .sendForm(
+          "gmail",
+          "template_apcrZw0l",
+          e.target,
+          "user_NN9nBpNBUHCW5dh5dryPi"
+        )
+        .then(
+          result => {
+            console.log("SUCCESS!", result.status, result.text);
+            var input = document.getElementsByClassName("input");
             input.forEach(element => {
               element.value = "";
             });
-        }, (error) => {
-            console.log('FAILED...', error);
-        });
+            var modal = document.getElementsByClassName("modal2");
+            modal.forEach(element => {
+              element.classList.add("is-active");
+            });
+          },
+          error => {
+            console.log("FAILED...", error);
+            var modal = document.getElementsByClassName("modal2");
+            document.getElementById('modaltitle').innerHTML ="Something went wrong... " + error;
+            modal.forEach(element => {
+              element.classList.add("is-active");
+            });
+          }
+        );
     }
-    
   }
-}
+};
 </script>
 
 <style scoped>
- .hero {
+.hero {
   background-image: url("../assets/pen.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   margin-left: 140px;
 }
-.title{
-  color:#364652;
+.title {
+  color: #364652;
 }
-.subtitle{
-  color:#364652;
+.subtitle {
+  color: #364652;
 }
-.modal-content{
-  width:800px;
+.modal-content {
+  width: 800px;
 }
-@media (max-width: 750px){
-  .hero{
-    margin-left:0; 
-    margin-top:17vh;
+@media (max-width: 750px) {
+  .hero {
+    margin-left: 0;
+    margin-top: 17vh;
   }
 }
 </style>
